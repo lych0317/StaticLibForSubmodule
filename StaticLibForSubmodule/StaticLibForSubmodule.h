ac626bc0d351df51d12a829d92f5bb3b236fa2ad
liyuanchao <liyuanchao@meituan.com>
2017-05-02 18:26:09 +0800

    添加name字段

---
+++ StaticLibForSubmodule/StaticLibForSubmodule.h
@@ -0,0 +1,15 @@
+//
+//  StaticLibForSubmodule.h
+//  StaticLibForSubmodule
+//
+//  Created by yuanchao Li on 2017/5/2.
+//  Copyright © 2017年 yuanchao Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StaticLibForSubmodule : NSObject
+
+@property (nonatomic, copy) NSString *name;
+
+@end
---
+++ StaticLibForSubmodule/StaticLibForSubmodule.m
@@ -0,0 +1,13 @@
+//
+//  StaticLibForSubmodule.m
+//  StaticLibForSubmodule
+//
+//  Created by yuanchao Li on 2017/5/2.
+//  Copyright © 2017年 yuanchao Li. All rights reserved.
+//
+
+#import "StaticLibForSubmodule.h"
+
+@implementation StaticLibForSubmodule
+
+@end
